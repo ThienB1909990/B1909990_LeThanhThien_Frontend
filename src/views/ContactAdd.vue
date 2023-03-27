227bf0eea6ce11511cbf584973ceeb7388566db3
ThienB1909990 <113990690+ThienB1909990@users.noreply.github.com>
2023-03-27 22:02:10 +0700

    Tao lien he moi

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="page">
+        <h4>Tạo liên hệ mới</h4>
+        <ContactForm :contact="contact" @submit:contact="AddContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {},
+            message: "",
+        };
+    },
+    methods: {
+        async AddContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Tạo thành công!!!";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -29,6 +29,13 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                }">
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -110,7 +117,7 @@ export default {
     mounted() {
         this.refreshList();
     },
-};      
+};
 
 </script>
 <style scoped>
